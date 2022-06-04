90d35c9bed7c6b20aa2d3393fc550aac4afddc78
bafread <106653277+bafread@users.noreply.github.com>
2022-06-04 20:38:56 +0200

    update main.tf
    1. Rename and add Firewall-rules with tags
    2. Change the image of gateway-vm to ubuntu with docker
    3. Build containerssh-guest-image on Sacrificial-vm

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,41 +23,68 @@ resource "google_compute_subnetwork" "honeypot_subnet" {
   network       = google_compute_network.main.self_link
 }
 
-resource "google_compute_firewall" "allow_ssh_in" {
-  name    = "allow-ssh-in"
+# standard firewall settings
+resource "google_compute_firewall" "firewall-all-allow-ssh" {
+  name    = "firewall-standard-rule"
   network = google_compute_network.main.self_link
 
   allow {
     protocol = "icmp"
   }
-
   allow {
     protocol = "tcp"
     ports    = ["22", "9091"]
   }
-
   source_ranges = ["0.0.0.0/0"]
 }
 
-resource "google_compute_firewall" "allow_node_exporter_e" {
-  name    = "allow-node-exporter-e"
+# open gateway-port 9100 and 9101, to our prometheus and metrics server
+resource "google_compute_firewall" "firewall-gateway-nodeexport" {
+  name    = "firewall-gateway-nodeexport"
   network = google_compute_network.main.self_link
 
   allow {
     protocol = "tcp"
-    ports    = ["9100"]
+    ports    = ["9100","9101"]
   }
 
-  source_ranges = ["10.0.0.0/24"]
+  target_tags = [ "gateway" ]
+  source_tags = [ "observer" ]
+}
+
+# allow inbound connection on TCP port 2376 from gateway
+resource "google_compute_firewall" "firewall-sacrificial-exception" {
+  name = "firewall-sacrificial-exception"
+  network = google_compute_network.main.name
+  priority = 500
+  source_tags = [ "gateway" ]
+  target_tags = [ "sacrificial" ]
+  allow {
+    protocol = "tcp"
+    ports = [ "2376" ]
+  }
+}
+
+# close all outgoing connection from sacrificial host
+resource "google_compute_firewall" "firewall-sacrificial-no-egress" {
+  name = "firewall-sacrificial-no-egress"
+  network = google_compute_network.main.name
+  direction = "EGRESS"
+  destination_ranges = [ "0.0.0.0/0" ]
+  target_tags = [ "sacrificial" ]
+  deny {
+    protocol = "all"
+  }
 }
 
 resource "google_compute_instance" "gateway_vm" {
   name         = "gateway-vm"
   machine_type = var.machine_type
+  tags = [ "gateway" ]
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-pro-cloud/ubuntu-pro-2204-lts"
+      image = "ubuntu-with-docker-image"
     }
   }
 
@@ -87,7 +114,7 @@ resource "google_compute_instance" "gateway_vm" {
 resource "google_compute_instance" "sacrificial_vm" {
   name         = "sacrificial-vm"
   machine_type = var.machine_type
-
+  tags = [ "sacrificial" ]
   boot_disk {
     initialize_params {
       image = "sacrificial-vm-image"
@@ -101,15 +128,28 @@ resource "google_compute_instance" "sacrificial_vm" {
 
     }
   }
+  connection {
+    type        = "ssh"
+    user        = "deployer"
+    private_key = file("./deployer_key")
+    host        = self.network_interface.0.access_config.0.nat_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo docker load -i /home/tmp/containerssh-guest-image.tar"
+    ]
+  }
+
 }
 
 resource "google_compute_instance" "logger_vm" {
   name         = "logger-vm"
   machine_type = var.machine_type
-
+  tags = [ "observer" ]
   boot_disk {
     initialize_params {
-      image = "logger-vm-image"
+      image = "ubuntu-with-docker-image"
     }
   }
 
