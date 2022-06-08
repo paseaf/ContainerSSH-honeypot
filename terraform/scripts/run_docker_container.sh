1ed9489584e60d9d32ac84e9153eed0b8a5c6d75
bafread <106653277+bafread@users.noreply.github.com>
2022-06-08 20:10:52 +0200

    just another version

--- terraform/scripts/run_docker_container.sh
+++ terraform/scripts/run_docker_container.sh
@@ -5,8 +5,7 @@ set -euxo pipefail
 export DEBIAN_FRONTEND=noninteractive
 
 # source: https://devopscube.com/run-docker-in-docker/
-sudo docker run -dit \
-   --name docker_container \
-   -v /var/run/docker.sock:/var/run/docker.sock \
+sudo docker run --privileged -d \
+   --name dind-container \
    -v /srv/containerssh/:/srv/containerssh/ \
-   docker
+   docker:dind
