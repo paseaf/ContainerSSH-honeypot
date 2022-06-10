212e25ec1758a8dc86cc0079e03ea2b436e82658
Ziyang Li <paseaf@me.com>
2022-06-10 11:48:01 +0200

    chore: add description to terraform variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,12 @@
-variable "project" {}
+variable "project" {
+  type = string
+  description = "Your GCP project ID"
+}
 
-variable "credentials" {}
+variable "credentials" {
+  type = string
+  description =  "Path to your GCP service account key file (JSON)"
+}
 
 variable "machine_type" {
   type    = string
