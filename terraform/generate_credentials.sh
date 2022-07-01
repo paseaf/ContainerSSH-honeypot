286ae4b1872e1bbb871649aaa92f0b149f5253fa
Ziyang Li <paseaf@me.com>
2022-07-01 16:37:27 +0200

    feat: exit if credential file already exists

--- terraform/generate_credentials.sh
+++ terraform/generate_credentials.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-set -euxo pipefail
+set -euo pipefail
 
 TARGET_FILE="./scripts/export_credentials.sh"
+
+# exit if file already exists
+if [ -f "$TARGET_FILE" ]; then
+   echo "[Error] file '$TARGET_FILE' already exists!" >&2
+   echo "[Error] Remove the file if you want to regenerate credentials" >&2
+   exit 1
+fi
+
+# set and generate credentials
 MINIO_ROOT_USER="ROOTNAME"
 MINIO_ROOT_PASSWORD=$(openssl rand -base64 32)
 
-echo "#!/bin/bash" >> "$TARGET_FILE"
-echo "set -euxo pipefail" >> "$TARGET_FILE"
-echo "MINIO_ROOT_USER=$MINIO_ROOT_USER" >> "$TARGET_FILE"
-echo "MINIO_ROOT_PASSWORD=\"$MINIO_ROOT_PASSWORD\"" >> "$TARGET_FILE"
+# write to file
+cat <<EOF >> "$TARGET_FILE"
+#!/bin/bash
+set -euxo pipefail
+
+export MINIO_ROOT_USER=$MINIO_ROOT_USER
+export MINIO_ROOT_PASSWORD="$MINIO_ROOT_PASSWORD"
+EOF
