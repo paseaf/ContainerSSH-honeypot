e81e1fc159be17ffabde78839940f23000699994
Ziyang Li <paseaf@me.com>
2022-07-02 13:31:41 +0200

    fix: gcloud failed with different default zone

--- terraform/scripts/move_certs_to_gateway_vm.sh
+++ terraform/scripts/move_certs_to_gateway_vm.sh
@@ -6,10 +6,14 @@ export DEBIAN_FRONTEND=noninteractive
 mkdir -p /tmp/ca
 
 # download key files from sacrificial VM
-gcloud compute scp --recurse deployer@sacrificial-vm:~/{ca,cert,key}.pem /tmp/ca/
+gcloud compute scp \
+   --recurse deployer@sacrificial-vm:~/{ca,cert,key}.pem /tmp/ca/ \
+   --zone=europe-west3-c
 
 # upload key files to gateway VM
-gcloud compute scp --recurse /tmp/ca deployer@gateway-vm:~/.docker
+gcloud compute scp \
+   --recurse /tmp/ca deployer@gateway-vm:~/.docker \
+   --zone=europe-west3-c
 
 # clean up temp files
 rm -rfv /tmp/ca
