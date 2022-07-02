8f0a8b5491bd6d864cb4cc6a0b2a59cabcbb7fbf
Ziyang Li <paseaf@me.com>
2022-07-02 12:13:00 +0200

    Pull contianers in Packer build

---
+++ packer/scripts/download_docker_images.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -euxo pipefail
+export DEBIAN_FRONTEND=noninteractive
+
+# cAdvisor
+REPO="google/cadvisor"
+VERSION=$(curl --silent "https://api.github.com/repos/$REPO/releases/latest" \
+  | grep '"tag_name":' \
+  | sed -E 's/.*"([^"]+)".*/\1/')
+
+docker pull gcr.io/cadvisor/cadvisor:"$VERSION"
+# rename for docker run later
+docker image tag gcr.io/cadvisor/cadvisor:"$VERSION" cadvisor:latest
+
+# Grafana
+docker pull  grafana/grafana
+
+# MinIO
+docker pull quay.io/minio/minio
+
+# Prometheus
+docker pull prom/prometheus
+
+# ContainerSSH
+docker pull containerssh/containerssh:0.4.1
+docker pull containerssh/containerssh-test-authconfig:0.4.1
