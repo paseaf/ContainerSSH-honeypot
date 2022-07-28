3f307eb464dbb1a479e6218e3fa242a12e646d01
Ziyang Li <paseaf@me.com>
2022-07-28 18:46:09 +0200

    fix wrong ExecStart override

--- terraform/scripts/restart_dockerd_with_tls.sh
+++ terraform/scripts/restart_dockerd_with_tls.sh
@@ -4,13 +4,15 @@ export DEBIAN_FRONTEND=noninteractive
 
 # Make docker daemon only accept connections with trusted certificate
 
-TARGET_DIR=/lib/systemd/system/local.conf
+TARGET_DIR=/lib/systemd/system/docker.service.d
 mkdir -p "$TARGET_DIR"
 
 # override default ExecStart
-cat > "$TARGET_DIR" << EOF
+cat > "$TARGET_DIR/local.conf" << EOF
 [Service]
-ExecStart= --tlsverify --tlscacert=/home/deployer/ca.pem \
+ExecStart=
+ExecStart=/usr/bin/dockerd -H fd:// --containerd=/run/containerd/containerd.sock \
+  --tlsverify --tlscacert=/home/deployer/ca.pem \
   --tlskey=/home/deployer/server-key.pem \
   --tlscert=/home/deployer/server-cert.pem \
   -H=0.0.0.0:2376
