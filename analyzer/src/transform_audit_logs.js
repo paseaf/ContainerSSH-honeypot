578a12bd33d91c06b6373800055026590205b2d6
Ziyang Li <paseaf@me.com>
2022-08-08 19:03:22 +0200

    add country code

---
+++ analyzer/src/transform_audit_logs.js
@@ -0,0 +1,23 @@
+/**
+ * @file transforms downloaded audit log for later use
+ */
+const fs = require("fs/promises");
+const geoip = require("geoip-country");
+const rawLogs = require("../downloads/object_list.json");
+
+// transform
+const transformedLogs = rawLogs.map(addCountry);
+
+// load
+fs.writeFile(
+  "./downloads/transformed_logs.json",
+  JSON.stringify(transformedLogs).split("},{").join("},\n{")
+);
+
+function addCountry(log) {
+  const country = geoip.lookup(log.ip).country;
+  return {
+    country,
+    ...log,
+  };
+}
