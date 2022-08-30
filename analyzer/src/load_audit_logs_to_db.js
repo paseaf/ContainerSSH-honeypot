bf6b968a7627ddc1176ede531043ce117e33b8a8
Ziyang Li <paseaf@me.com>
2022-08-30 19:36:50 +0200

    refactor: extract loadMetadataToDb

--- analyzer/src/load_audit_logs_to_db.js
+++ analyzer/src/load_audit_logs_to_db.js
@@ -6,9 +6,16 @@ const DB_FILENAME = "audit_log.db";
 const db = new sqlite3.Database(DB_FILENAME);
 const transformedLogs = require("../downloads/transformed_audit_log_metadata.json");
 
-db.serialize(() => {
-  // Create audit_log table
-  db.run(`CREATE TABLE IF NOT EXISTS audit_log (
+main();
+function main() {
+  loadMetadataToDb();
+  loadAuditLogsToDb();
+}
+
+function loadMetadataToDb() {
+  db.serialize(() => {
+    // Create audit_log table
+    db.run(`CREATE TABLE IF NOT EXISTS audit_log (
     name TEXT PRIMARY KEY NOT NULL, 
     byteSize INTEGER NOT NULL,
     lastModified STRING NOT NULL,
@@ -18,33 +25,34 @@ db.serialize(() => {
     country TEXT
     )`);
 
-  // Inserting to table
-  db.run("BEGIN TRANSACTION");
-  const stmt = db.prepare(
-    "INSERT OR REPLACE INTO audit_log VALUES (?,?,?,?,?,?,?)"
-  );
-  transformedLogs.forEach((log, idx) => {
-    stmt.run([
-      log.name,
-      log.byteSize,
-      log.lastModified,
-      log.ip,
-      log.isAuthenticated,
-      log.username,
-      log.country,
-    ]);
-    console.log(`Inserting progress: ${idx + 1}/${transformedLogs.length}`);
-  });
-  stmt.finalize();
-  db.run("COMMIT");
-
-  // Verification
-  db.get("SELECT COUNT(*) AS count FROM audit_log", (_err, result) => {
-    console.log(
-      `Loading finished.
-Inserted ${result.count} rows to "./${DB_FILENAME}"`
+    // Inserting to table
+    db.run("BEGIN TRANSACTION");
+    const stmt = db.prepare(
+      "INSERT OR REPLACE INTO audit_log VALUES (?,?,?,?,?,?,?)"
     );
-  });
-});
+    transformedLogs.forEach((log, idx) => {
+      stmt.run([
+        log.name,
+        log.byteSize,
+        log.lastModified,
+        log.ip,
+        log.isAuthenticated,
+        log.username,
+        log.country,
+      ]);
+      console.log(`Inserting progress: ${idx + 1}/${transformedLogs.length}`);
+    });
+    stmt.finalize();
+    db.run("COMMIT");
 
-db.close();
+    // Verification
+    db.get("SELECT COUNT(*) AS count FROM audit_log", (_err, result) => {
+      console.log(
+        `Loading finished.
+Inserted ${result.count} rows to "./${DB_FILENAME}"`
+      );
+    });
+  });
+
+  db.close();
+}
