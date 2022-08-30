c7f69fde1c8a51db00ccd54e8f49d331a2daeaa3
Ziyang Li <paseaf@me.com>
2022-08-30 19:34:33 +0200

    feat: parse decoded logs and extract data

--- analyzer/src/transform_audit_logs.js
+++ analyzer/src/transform_audit_logs.js
@@ -7,11 +7,11 @@ const rawMetadata = require("../downloads/downloaded_audit_log_metadata.json");
 
 const { spawn } = require("node:child_process");
 const { open, readdir } = require("node:fs/promises");
+const readline = require("node:readline");
 const { assert } = require("console");
 main().catch((e) => console.error(e));
 
 async function main() {
-  await transformAuditLogs();
   // transform metadata
   const transformedMetadata = rawMetadata.map(addCountry);
 
@@ -20,6 +20,9 @@ async function main() {
     "./downloads/transformed_audit_log_metadata.json",
     JSON.stringify(transformedMetadata).split("},{").join("},\n{")
   );
+
+  await decodeAuditLogs();
+  await parseDecodedAuditLogs();
 }
 
 function addCountry(log) {
@@ -30,7 +33,7 @@ function addCountry(log) {
   };
 }
 
-async function transformAuditLogs() {
+async function decodeAuditLogs() {
   const objectDir = "./downloads/objects";
   const objectNames = (await readdir(objectDir)).filter(
     (filename) => filename.length === 32
@@ -63,10 +66,10 @@ async function transformAuditLogs() {
 
     decoder.on("close", (exitCode) => {
       if (exitCode === 0) {
+        currentProgress++;
         console.log(
           `${objectName} decoded! Progress: ${currentProgress}/${objectNames.length}`
         );
-        currentProgress++;
         fd?.close();
       } else {
         console.error(
@@ -85,4 +88,81 @@ async function transformAuditLogs() {
 Number of original objects: ${objectNames.length}
 Number of decoded objects: ${decodedObjectNames.length}`
   );
+}
+
+/**
+ * Parse decoded audit logs and extract parts of data for analysis
+ */
+async function parseDecodedAuditLogs() {
+  // decoding
+  const sourceDir = "./downloads/objects_decoded";
+  const sourceFileNames = (await readdir(sourceDir)).filter(
+    (filename) => filename.length === 32 + ".json".length
+  );
+
+  const targetFilePath = "./downloads/objects_parsed.json";
+  const targetFd = await open(targetFilePath, "w");
+  const targetWritable = targetFd.createWriteStream({
+    encoding: "utf8",
+  });
+
+  let currentProgress = 0;
+  for (const sourceFileName of sourceFileNames) {
+    const sourceFd = await open(`${sourceDir}/${sourceFileName}`, "r");
+    const sourceReadable = sourceFd.createReadStream({
+      encoding: "utf8",
+    });
+    const rl = readline.createInterface({
+      input: sourceReadable,
+      crlfDelay: Infinity,
+    });
+
+    const parsedObject = {
+      password: undefined,
+      commands: [],
+      startTimestamp: 0,
+      endTimestamp: 0,
+    };
+    console.log(`Parsing ${sourceFileName}...`);
+
+    rl.on("line", (line) => {
+      const obj = JSON.parse(line);
+      switch (obj.typeId) {
+        case "auth_password": {
+          const passwordBuffer = Buffer.from(obj.payload.password, "base64");
+          parsedObject.password = passwordBuffer.toString();
+          break;
+        }
+        case "exec": {
+          const command = {
+            command: obj.payload.program,
+            timestamp: obj.timestamp,
+          };
+          parsedObject.commands.push(command);
+          break;
+        }
+        case "connect":
+          parsedObject.startTimestamp = obj.timestamp;
+          break;
+        case "disconnect":
+          parsedObject.endTimestamp = obj.timestamp;
+          break;
+        default:
+        // ignore other commands
+      }
+    });
+
+    rl.on("close", () => {
+      // append parsed object to a file?
+      targetWritable.write(JSON.stringify(parsedObject));
+      targetWritable.write("\n");
+
+      // update states
+      currentProgress++;
+      console.log(
+        `${sourceFileName} parsed! Progress: ${currentProgress}/${sourceFileNames.length}`
+      );
+      sourceFd?.close();
+    });
+  }
 }
