1d93349c59a30a5fe8b458b57254bbb9f9de3a0b
Ziyang Li <paseaf@me.com>
2023-02-17 09:29:56 +0100

    improve packer var conig

--- packer/run.sh
+++ packer/run.sh
@@ -2,8 +2,9 @@
 
 set -euxo pipefail
 
-packer validate .
+PROJECT_ID=$(gcloud config get project)
+packer validate -var "project_id=$PROJECT_ID" .
 packer fmt .
 
 # Note: use `-force` to overwrite previously built image
-packer build -force .
+packer build -force -var "project_id=$PROJECT_ID" .
